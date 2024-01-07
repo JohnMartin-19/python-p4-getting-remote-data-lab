16ff72bb87a82458b25731a556bb4a985cfdb13c
John <johnnkonge2020@gmail.com>
2024-01-07 14:01:10 +0300

    get request done!!

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,20 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
-        self.url = url
 
     def get_response_body(self):
-        pass
+        url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        all_data = []
+        data = json.loads(self.get_response_body())
+        for d in data:
+            all_data.append(d)
+
+        return all_data
+my_data = GetRequester()
+
+print(my_data.load_json())
+
